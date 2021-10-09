c54b068f367610e349c65adfaed4c0bd9c4ef24b
Orcco99 <orccotom@sheridancollege.ca>
2021-10-09 01:22:42 -0400

    Added card object to CardTrick class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,14 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
+ * Name: Oscar Orccotoma
+ * Student number: 991622081
  * @author Ronak
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card a1 = new Card(); // new card object
     }
     
 }
